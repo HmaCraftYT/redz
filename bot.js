b80eb0e798f9a4b9bb1d8a1b156f3644cf08de42
HmaCraftYT <45132766+HmaCraftYT@users.noreply.github.com>
2018-12-10 13:40:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "!";
 var adminprefix = '!'
-
+const developers = ["448223958228140044","id"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
